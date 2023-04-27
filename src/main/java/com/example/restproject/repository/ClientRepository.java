bf734f17e3b557a33b56debb256445c0ce7f324f
Timur <timkonovalov9858@gmail.com>
2023-04-27 15:33:42 +0300

    ClientServise interface delete

--- src/main/java/com/example/restproject/repository/ClientRepository.java
+++ src/main/java/com/example/restproject/repository/ClientRepository.java
@@ -4,5 +4,6 @@ package com.example.restproject.repository;
 import com.example.restproject.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/* Интерфейс для взаимодействия с БД. Предоставляет возможность сохранять в удобном виде Java-объекты в базе данных. */
+/* Интерфейс для взаимодействия с БД.
+Предоставляет возможность сохранять в удобном виде Java-объекты в базе данных. */
 public interface ClientRepository extends JpaRepository<Client, Integer> {}
