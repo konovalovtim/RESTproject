a0288c8c1e3921ed9e0f5a43984aa028fdf85cb1
Timur <timkonovalov9858@gmail.com>
2023-04-27 15:42:11 +0300

    add comments

--- src/main/java/com/example/restproject/repository/ClientRepository.java
+++ src/main/java/com/example/restproject/repository/ClientRepository.java
@@ -5,5 +5,5 @@ import com.example.restproject.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /* Интерфейс для взаимодействия с БД.
-Предоставляет возможность сохранять в удобном виде Java-объекты в базе данных. */
+Предоставляет возможность сохранять в удобном виде Java-объекты в базе данных(связь Java объектов и записей в БД) */
 public interface ClientRepository extends JpaRepository<Client, Integer> {}
