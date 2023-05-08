addecf07cf5b917cb03e89393716aa80da1c87ae
Timur <timkonovalov9858@gmail.com>
2023-05-08 10:38:17 +0300

    slf4j logger add

--- target/generated-sources/annotations/com/example/restproject/mapper/ClientMapperImpl.java
+++ target/generated-sources/annotations/com/example/restproject/mapper/ClientMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-07T15:28:16+0300",
+    date = "2023-05-08T10:37:19+0300",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
